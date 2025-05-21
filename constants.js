1b9a84a7b45d11e584e3ef8eab8a66a34a026d32
Michael Kapelko <kornerr@gmail.com>
2025-05-21 19:33:57 +0300

    Prepare three panels of the editor (#1)

---
+++ constants.js
@@ -0,0 +1,5 @@
+const TEMPLATE_EDITOR_PANELS = `
+    <header id="panel-header">TODO: header</header>
+    <div id="panel-left">TODO: left</div>
+    <div id="panel-main"><p>TODO: main</p><div>
+`;
---
+++ other.js
@@ -0,0 +1,4 @@
+// Delimit document body into three parts.
+function setupEditorPanels() {
+    document.body.innerHTML = TEMPLATE_EDITOR_PANELS;
+}
