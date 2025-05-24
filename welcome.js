859415a8a32fcd16509792d5adb8cd1e258f93d3
Михаил Капелько <kornerr@gmail.com>
2025-05-24 22:02:14 +0300

    d

--- welcome.js
+++ welcome.js
@@ -1,1 +1,20 @@
-console.log("欢迎光临");
+//<!-- Setup -->
+window.addEventListener("load", (e) => {
+    welcomeSetup();
+});
+
+//<!-- Constants -->
+
+TEMPLATE_WELCOME_HEADER = `<h1>Welcome</h1>`;
+
+//<!-- Context -->
+
+
+//<!-- Shoulds -->
+
+
+//<!-- Other -->
+
+function welcomeSetup() {
+    console.log("欢迎光临");
+}
