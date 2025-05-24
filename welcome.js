157479d50f3df7916e94d7e7da04b5d6eaee3257
Михаил Капелько <kornerr@gmail.com>
2025-05-24 22:30:54 +0300

    d

--- welcome.js
+++ welcome.js
@@ -6,6 +6,15 @@ window.addEventListener("load", (e) => {
 //<!-- Constants -->
 
 TEMPLATE_WELCOME_HEADER = `<div class="uk-position-center-vertical uk-padding">Welcome to PSKOV2</div>`;
+TEMPLATE_WELCOME_LEFT = `
+<div>
+    <ul class="uk-nav uk-nav-default">
+        <li class="uk-nav-header">Welcome section</li>
+        <li class="uk-nav-divider"></li>
+        <li><a href="#"><span uk-icon="triangle-right"></span>What is PSKOV</a></li>
+        <li><a href="#">What PSKOV is not</a></li>
+    </ul>
+</div>`;
 
 //<!-- Context -->
 
@@ -18,5 +27,7 @@ TEMPLATE_WELCOME_HEADER = `<div class="uk-position-center-vertical uk-padding">W
 function welcomeSetup() {
     console.log("欢迎光临");
     let header = document.getElementById("panel-header");
-    header.innerHTML = TEMPLATE_WELCOME_HEADER;
+    header.innerHTML += TEMPLATE_WELCOME_HEADER;
+    let left = document.getElementById("panel-left");
+    left.innerHTML += TEMPLATE_WELCOME_LEFT;
 }
