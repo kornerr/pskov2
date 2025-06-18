194fad0541f105f82d575c4ed18e84acf581a428
Михаил Капелько <kornerr@gmail.com>
2025-06-18 22:57:42 +0300

    display files of the repo

--- fs.js
+++ fs.js
@@ -9,9 +9,11 @@ function fs() {
 function FSContext() {
     this._construct = function() {
         this.didLaunch = false;
+        this.htmlFiles = "";
         this.selectedItemId = -1;
         this.sideId = -1;
         this.sideSelectedItemId = -1;
+        this.walkedFiles = [];
 
         this.recentField = "";
     };
@@ -20,12 +22,16 @@ function FSContext() {
     this.field = function(name) {
         if (name == "didLaunch") {
             return this.didLaunch;
+        } else if (name == "htmlFiles") {
+            return this.htmlFiles;
         } else if (name == "selectedItemId") {
             return this.selectedItemId;
         } else if (name == "sideId") {
             return this.sideId;
         } else if (name == "sideSelectedItemId") {
             return this.sideSelectedItemId;
+        } else if (name == "walkedFiles") {
+            return this.walkedFiles;
         }
 
         return "unknown-field-name";
@@ -34,9 +40,11 @@ function FSContext() {
     this.selfCopy = function() {
         let that = new FSContext();
         that.didLaunch = this.didLaunch;
+        that.htmlFiles = this.htmlFiles;
         that.selectedItemId = this.selectedItemId;
         that.sideId = this.sideId;
         that.sideSelectedItemId = this.sideSelectedItemId;
+        that.walkedFiles = this.walkedFiles;
 
         that.recentField = this.recentField;
         return that;
@@ -45,12 +53,16 @@ function FSContext() {
     this.setField = function(name, value) {
         if (name == "didLaunch") {
             this.didLaunch = value;
+        } else if (name == "htmlFiles") {
+            this.htmlFiles = value;
         } else if (name == "selectedItemId") {
             this.selectedItemId = value;
         } else if (name == "sideId") {
             this.sideId = value;
         } else if (name == "sideSelectedItemId") {
             this.sideSelectedItemId = value;
+        } else if (name == "walkedFiles") {
+            this.walkedFiles = value;
         }
     };
 }
@@ -63,7 +75,7 @@ let FS_PAGES = {
   0: `
 <div class="uk-container uk-padding">
     <h1 class="uk-heading">Files</h1>
-    <p>TODO tree of files</P>
+    %FILES%
 </div>
 `,
 };
@@ -86,16 +98,30 @@ function FSComponent() {
         this.setupShoulds();
     };
 
+    this.resetFiles = function() {
+        (async() => {
+            var files = [];
+            await fsWalkFiles(this.pfs, "/", files);
+            this.ctrl.set("walkedFiles", files);
+        })();
+    };
+
     this.setupEffects = function() {
-        this.ctrl.registerFieldCallback("selectedItemId", (c) => {
+        this.ctrl.registerFieldCallback("htmlFiles", (c) => {
             let contents = FS_PAGES[c.selectedItemId];
             let main = deId(FS_PANEL_MAIN);
-            main.innerHTML = contents;
+            main.innerHTML = contents
+                .replaceAll("%FILES%", c.htmlFiles);
+        });
+
+        this.ctrl.registerFieldCallback("selectedItemId", (c) => {
+            this.resetFiles();
         });
     };
 
     this.setupShoulds = function() {
         [
+            fsShouldResetHTMLFiles,
             fsShouldResetSelectedItemId,
         ].forEach((f) => {
             this.ctrl.registerFunction(f);
@@ -125,6 +151,32 @@ function FSComponent() {
 //<!-- Shoulds -->
 
 // Conditions:
+// 1. Files side menu item has been selected
+// 2. Files have been read
+function fsShouldResetHTMLFiles(c) {
+    if (c.recentField == "selectedItemId") {
+        c.htmlFiles = "<p>Loading...</p>";
+        c.recentField = "htmlFiles";
+        return c;
+    }
+
+    if (c.recentField == "walkedFiles") {
+        var html = "<ul>";
+        for (let i in c.walkedFiles) {
+            let file = c.walkedFiles[i];
+            html += `<li>${file}</li>`;
+        }
+        html += "</ul>";
+        c.htmlFiles = html;
+        c.recentField = "htmlFiles";
+        return c;
+    }
+
+    c.recentField = "none";
+    return c;
+}
+
+// Conditions:
 // 1. Side menu item has been selected
 function fsShouldResetSelectedItemId(c) {
     if (
@@ -151,6 +203,23 @@ function fsIsSideSelectionRelevant(selectedItemId, sideId) {
     }
 
     return false;
+}
+
+// Collect a list of directories and files into the provided `collection`
+async function fsWalkFiles(pfs, path, collection) {
+    var suffix = path == "/" ? "" : "/";
+    collection.push(path + suffix);
+    var files = await pfs.readdir(path)
+    for (var i in files) {
+        var f = files[i];
+        var fullPath = path + suffix + f;
+        var st = await pfs.stat(fullPath);
+        if (st.type == "dir") {
+            await fsWalkFiles(pfs, fullPath, collection);
+        } else {
+            collection.push(fullPath);
+        }
+    }
 }
 
 //<!-- Setup -->
--- other.js
+++ other.js
@@ -2,3 +2,8 @@
 function deId(id) {
     return document.getElementById(id);
 }
+
+// Wait for the specified duration asynchronously
+async function asyncSleep(duration) {
+    await new Promise((resolve) => setTimeout(resolve, duration));
+}
