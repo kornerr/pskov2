4012d30d8b0de6d2c8d54358324940b5dd03c4bb
Михаил Капелько <kornerr@gmail.com>
2025-08-23 23:11:08 +0300

    add Xcode generated macOS project

---
+++ mac/PSKOV/PSKOV/PSKOVApp.swift
@@ -0,0 +1,17 @@
+//
+//  PSKOVApp.swift
+//  PSKOV
+//
+//  Created by Michael Kapelko on 23.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct PSKOVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
